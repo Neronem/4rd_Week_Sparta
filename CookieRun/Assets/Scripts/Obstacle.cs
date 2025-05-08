5e7270663b636bdbb72f79902920fc59bcd91abf
smg583 <rekoof3558@naver.com>
2025-05-08 14:56:51 +0900

    feat : 장애물 생성
    랜덤 포지셔닝 로직 생성

--- CookieRun/Assets/Scripts/Obstacle.cs
+++ CookieRun/Assets/Scripts/Obstacle.cs
@@ -6,7 +6,7 @@ public class Obstacle : MonoBehaviour
 {
     Collision2D collision;
 
-    float spacing = 0;
+    float widthPadding;
 
     void Start()
     {
@@ -15,21 +15,27 @@ public class Obstacle : MonoBehaviour
 
     void Update()
     {
-        
+        widthPadding = Mathf.Lerp(10, 3, Time.deltaTime);
     }
 
-    //Vector3 RandomPosition(Vector3 lastPosition)
-    //{
-    //    float[] Y = { 0, 100, 200 };
-    //    float posY = Y[Random.Range(0, Y.Length)];
+    Vector3 RandomPosition(Vector3 lastPosition)
+    {
+        float[] Y = { 0, 100, 200 };
+        float posY = Y[Random.Range(0, Y.Length)];
 
-    //    float posX = lastPosition.x - spacing;
+        Vector3 place = lastPosition + new Vector3(widthPadding, 0);
+        place.y = posY;
 
-
-    //}
+        transform.position = place;
+        return place;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Hit
+        //Player player = collision.GetComponent<Player>();
+        //if (player != null)
+        //{
+        //    Hit
+        //}
     }
 }
