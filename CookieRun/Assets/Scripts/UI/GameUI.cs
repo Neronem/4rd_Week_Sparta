bb54d86cd4dea5f492d8281050cd13a7e26be34f
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-09 17:27:42 +0900

    feat : 게임, 게임오버 UI 외형 구현

--- CookieRun/Assets/Scripts/UI/GameUI.cs
+++ CookieRun/Assets/Scripts/UI/GameUI.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameUI : BaseUI
+public class GameUI : MonoBehaviour
 {
-    protected override UIState GetUIState()
+    public TextMeshProUGUI currentScoreText; // 현재 점수
+    public TextMeshProUGUI bestScoreText; // 최고 점수
+    
+    [SerializeField] private Slider healthSlider; // 슬라이더 컴포넌트
+    [SerializeField] private float lerpSpeed = 5f; // 부드럽게 움직이는 속도
+    
+    private PlayerController playerController; // PlayerController 참조
+    private float targetHealth; // Lerp에 사용될 플레이어 체력 저장 변수
+    
+    void Start()
     {
-        return UIState.Game;
+        // PlayerController 스크립트 찾아서 할당
+        playerController = FindObjectOfType<PlayerController>();
+        healthSlider = transform.Find("HealthBar").GetComponent<Slider>();
+        currentScoreText = transform.Find("CurrentScoreText").GetComponent<TextMeshProUGUI>(); 
+        bestScoreText = transform.Find("BestScoreText").GetComponent<TextMeshProUGUI>();
+        
+        healthSlider.maxValue = playerController.playermaxhealth; // 슬라이더의 최대 값 설정
+        healthSlider.value = playerController.playermaxhealth;
     }
 
-    public override void Init(UIManager manager)
+    void Update()
     {
-        base.Init(manager);
+        // currentScoreText.text = score; 등 구현 필요
+        
+        targetHealth = playerController.currenthealth; // 프레임마다 플레이어 체력 가져오고
+        healthSlider.value = Mathf.Lerp(healthSlider.value, targetHealth, lerpSpeed * Time.deltaTime); // 체력바의 체력 부드럽게 전환
     }
-    
 }
