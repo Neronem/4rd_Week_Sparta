4e9371498f499ac60b3e9fb56e7ff5273e172e6c
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-09 14:37:55 +0900

    Add : UI 스크립트 추가

--- CookieRun/Assets/Scripts/UI/GameOverUI.cs
+++ CookieRun/Assets/Scripts/UI/GameOverUI.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverUI : MonoBehaviour
+public class GameOverUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override UIState GetUIState()
     {
-        
+        return UIState.GameOver;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Init(UIManager manager)
     {
-        
+        base.Init(manager);
     }
 }
