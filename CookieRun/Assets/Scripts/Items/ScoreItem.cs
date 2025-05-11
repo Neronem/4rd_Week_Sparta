ad28201cca70d8aad5f3eb4eafef820009742f98
codingnewwbie <abyss6715@gmail.com>
2025-05-11 20:51:16 +0900

    update: 주석 추가

--- CookieRun/Assets/Scripts/Items/ScoreItem.cs
+++ CookieRun/Assets/Scripts/Items/ScoreItem.cs
@@ -7,6 +7,6 @@ public class ScoreItem : BaseItem
     protected override void HandlePlayerCollision(GameObject player)
     {
         GameManager.Instance.AddScore(itemData.Score); // itemData에서 지정.
-        Destroy(gameObject);
+        Destroy(gameObject); // 습득한 아이템 파괴
     }
 }
