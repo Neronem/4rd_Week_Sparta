d6f47b907bd54f516ae3fb9c83785c44c124cd2b
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-12 11:36:14 +0900

    feat : 게임 시작화면

--- CookieRun/Assets/Scripts/GameManager.cs
+++ CookieRun/Assets/Scripts/GameManager.cs
@@ -139,7 +139,7 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isGameOver = true;
-        UIManager.instance.GameUIDisappear();
-        UIManager.instance.GameOverUIAppear();
+        GameUIManager.instance.GameUIDisappear();
+        GameUIManager.instance.GameOverUIAppear();
     }
 }
